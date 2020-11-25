2fb4f571c7f88f7fc1b809501f217eac8a6a1bd3
fletech <facundogarcia.ar@gmail.com>
2020-11-25 23:54:54 +0100

    formulario carga de producto con MULTER

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -15,14 +15,38 @@ let productosController = {
     crearView : function (req, res){ //Ruta por GET para llegar a la carga.
         res.render ('crearProducto', { title: 'Caniada - Crear Producto'})
     },
+
     crear : function (req, res){ //Ruta por POST al enviar formulario.
-        productosDB.push(req.body)
-        console.log(req.body)
-        fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productosDB, null, 4))
-        res.redirect ('/productos/crear/success')
+
+        let productoCargado = {
+            categoria: req.body.categoria,
+            genero: req.body.genero ,
+            nombre: req.body.nombre,
+            color: req.body.color,
+            talle: req.body.talle,
+            cantidad: req.body.cantidad,
+            precio: req.body.precio,
+            descripcion: req.body.descripcion,
+            modelo: req.body.modelo,
+            composicion: req.body.composicion,
+            sku: req.body.sku,
+            fotoProducto: req.file.filename}
+        
+            productosDB.push(productoCargado);
+            console.log(req.files);
+
+        fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productosDB, null, 4));
+
+        res.redirect('/productos/crear/success')
     },
+
     success : function (req, res){
         res.render('crearProductoSuccess', {title: 'Caniada - Producto creado'})
+    },
+
+    detalle: function (req, res) {
+        
+        res.render('detalleProducto', {productosDB: productosDB, title : productosDB[i].nombre})
     }
 
 }
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,10 +1,28 @@
-let express = require ('express')
+let express = require ('express');
+const multer = require ('multer');
+const path = require ('path');
+const productosController = require ('../controllers/productosController');
+
 let router = express.Router()
-const productosController = require ('../controllers/productosController')
+
+var storage = multer.diskStorage({
+     destination: function (req, file, cb) {
+       cb(null, path.join(__dirname,'../../public/images/fotosProductos'))
+     },
+     filename: function (req, file, cb) {
+          
+          
+       cb(null, req.body.nombre + '-' + Date.now() + path.extname(file.originalname))
+     }
+   })
+    
+   var upload = multer({ storage: storage })
 
 router.get('/', productosController.productos ) //funciona OK
 router.get ('/crear', productosController.crearView) //funciona OK
-router.post ('/crear', productosController.crear)
+router.post ('/crear', upload.single('fotoProducto'),productosController.crear)
 router.get ('/crear/success', productosController.success)
+router.get ('/:sku', productosController.detalle)
 
 module.exports = router
+
