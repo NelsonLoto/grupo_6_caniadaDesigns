301479108e74d1f7c7f16efef7050230e195320c
fletech <facundogarcia.ar@gmail.com>
2020-11-25 21:18:09 +0100

    .

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -12,11 +12,12 @@ let productosController = {
     productos: function (req, res) {
         res.render('detalleproducto',  { title: 'Caniada - Productos' })
     },
-    crearView : function (req, res){
+    crearView : function (req, res){ //Ruta por GET para llegar a la carga.
         res.render ('crearProducto', { title: 'Caniada - Crear Producto'})
     },
-    crear : function (req, res){
+    crear : function (req, res){ //Ruta por POST al enviar formulario.
         productosDB.push(req.body)
+        console.log(req.body)
         fs.writeFileSync(path.join(__dirname, '../database/productos.json'), JSON.stringify(productosDB, null, 4))
         res.redirect ('/productos/crear/success')
     },
