bdce22320c68ca948503e67b80c02d97716b2be6
Juan Pablo Jepik <juanpablojepik@MacBook-Air-de-Juan.local>
2021-01-17 18:24:29 -0300

    Raiz /database corregida

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,8 +4,7 @@ module.exports = {
     "password": null,
     "database": "database_development",
     "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": "3000"
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
