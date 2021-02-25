418df666ac12b49b95b24c321e7e6a651a26606e
guidoMagaldi <guidoMagaldi>
2021-02-25 16:53:24 -0300

    cambios en raq queries

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -3,57 +3,39 @@ const Sequelize = require ('sequelize')
 
 let apiController = {
     productos: function(req,res){
-        db.Categoria.findAll({ 
-            attributes: { 
-                include: [[Sequelize.fn("COUNT", Sequelize.col("id_categoria")), "n_productos"]] 
-            },
-            include: [{
-                model: db.Producto,
-                as: 'productos',
-                 attributes: [],
+        db.Producto.findAll()
+        .then(function(resultados){
+            db.Categoria.findAll({ include: { all: true }})
+        .then(function(categorias){
+            let productosPorCategoria = {}
+
+            for (let i = 0; i < categorias.length; i++) {
+                productosPorCategoria[categorias[i].nombre_categoria] = categorias[i].productos.length
                 
-            }],
-            group: ['id']
-        }).then(function(resultados){
-            let categorias = []
-            for (let i = 0; i < resultados.length; i++) {
-                categorias.push({
-                    Categoria: resultados[i].nombre_categoria,
-                    productos: resultados[i].n_productos
-                })
+            
             }
-            db.Producto.findAll()
-            .then(function(Productos){
-                let arreglo = []
-                for(let i=0; i<Productos.length; i++){
+            // categorias.forEach(element => {
+            //     productosPorCategoria[element.nombre_categoria] = element.productos.length
+            // });
+            let arreglo = []
+                for(let i=0; i<resultados.length; i++){
                     arreglo.push({
-                        id:Productos[i].id_producto,
-                        name: Productos[i].nombre,
-                        description: Productos[i].descripcion,
-                        detail: `/productos/${Productos[i].sku}`
+                        id:resultados[i].id_producto,
+                        name: resultados[i].nombre,
+                        description: resultados[i].descripcion,
+                        detail: `/productos/${resultados[i].nombre}`
                     })
-                }
-                res.json({
-                    resultados:categorias,
-                    count: Productos.length,
-                    Ruta: arreglo,
-                    Productos: Productos
                    
-                })
-            })
+                }
+                 
+                res.json({
+                    Count: resultados.length,
+                    countByCategory: productosPorCategoria,
+                    Products: arreglo
+                    
         })
-    }
-}
-
-    /* detalle: function(req,res){
-        db.Producto.findByPk(req.params.id)
-        .then(function(productoDetail){
-            res.json({
-                product: productoDetail,
-                url: `/public/images/fotosProductos/${productoDetail.imagen_1}`
-            })
-        })
-    } */
-
+    })
+})
+    }}
 
 module.exports = apiController;
