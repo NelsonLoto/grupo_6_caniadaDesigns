f4a5ca638ccb9a252de22260a70393c3f4cec8c8
guidoMagaldi <guidoMagaldi>
2021-03-12 20:47:29 -0300

    Menu dropdown listo para poder hacer el carrito

--- public/js/agregarCarrito.js
+++ public/js/agregarCarrito.js
@@ -1,14 +1,27 @@
-//funcion
+// //funcion
 
-let agregarCarrito = document.getElementsByClassName('whislistOverlay')
- console.log(agregarCarrito);
+// let agregarCarrito = document.getElementsByClassName('whislistOverlay')
+//  console.log(agregarCarrito);
 
-// agregarCarrito.addEventListener('click', ()=> {
-//     // console.log("lo atrapamos");
-//     // localStorage.setItem('Producto',productosDB[i].id_producto )
-//     // console.log(localStorage.getItem("Producto"));
-//     let id = agregarCarrito.querySelector('p')
+// // agregarCarrito.addEventListener('click', ()=> {
+// //     // console.log("lo atrapamos");
+// //     // localStorage.setItem('Producto',productosDB[i].id_producto )
+// //     // console.log(localStorage.getItem("Producto"));
+// //     let id = agregarCarrito.querySelector('p')
 
-//     console.log(agregarCarrito)
+// //     console.log(agregarCarrito)
 
-// })
+// // })
+
+
+// function levantaID(){        
+//       let elemento = document.getElementsByClassName('elItem');
+//       let array_id = [];          
+//       for(let i = 0; i < elemento.length; i++){             
+//       let id = elemento[i].getAttribute('id');
+//                      array_id.push(id);
+//                  }        
+//                      console.log(array_id);   
+//                      localStorage.setItem('datos', JSON.stringify(array_id));
+//                      // Guardo el objeto como un string en Storage } //End function levantaID
+
