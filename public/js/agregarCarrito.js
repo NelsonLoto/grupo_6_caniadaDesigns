706eafcfbbf6c169643263d66d77282f028215d5
guidoMagaldi <guidoMagaldi>
2021-03-12 17:46:28 -0300

    haciendo carrito parte uno

---
+++ public/js/agregarCarrito.js
@@ -0,0 +1,14 @@
+//funcion
+
+let agregarCarrito = document.getElementsByClassName('whislistOverlay')
+ console.log(agregarCarrito);
+
+// agregarCarrito.addEventListener('click', ()=> {
+//     // console.log("lo atrapamos");
+//     // localStorage.setItem('Producto',productosDB[i].id_producto )
+//     // console.log(localStorage.getItem("Producto"));
+//     let id = agregarCarrito.querySelector('p')
+
+//     console.log(agregarCarrito)
+
+// })
