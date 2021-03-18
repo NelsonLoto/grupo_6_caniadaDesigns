50f947bb632a5f1d83d44939bc4e818f4af5aff7
guidoMagaldi <guidoMagaldi>
2021-03-18 19:26:06 -0300

    agregar carrito

--- public/js/agregarCarrito.js
+++ public/js/agregarCarrito.js
@@ -3,12 +3,12 @@
     document.querySelectorAll('.agregarCarrito').forEach(item => {
         
         item.addEventListener('click', () => {
-          console.log(item.dataset.myvalue);
+            
           var existing = localStorage.getItem('Producto');// traigo la data que exista hasta el momento
           existing = existing ? existing.split(',') : []; //si no existe data, hago un array, y si es un array lo hago string.
           existing.push(item.dataset.myvalue); //agrego nueva data al array
 
-          localStorage.setItem('Producto', existing.toString()); //lo guardo de nuevo en local storage
+         localStorage.setItem('Producto', existing.toString()); //lo guardo de nuevo en local storage
 
 
           
